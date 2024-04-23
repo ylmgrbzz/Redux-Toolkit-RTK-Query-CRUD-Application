ca307c59db58ccfc9ce12f8dd1406c554372a8a9
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-04-23 18:15:33 +0300

    Update ContactsApi to use Contact model and fix formatting

--- src/services/ContactsApi.tsx
+++ src/services/ContactsApi.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-
+import { Contact } from "../models/contact.model";
 
 export const contactsApi = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/" }),
-    endpoints: (builder) => ({
-        contacts: builder.query({
-            query: () => "contacts",
-        }),
+  reducerPath: "contactsApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/" }),
+  endpoints: (builder) => ({
+    contacts: builder.query<Contact[], void>({
+      query: () => "/contacts",
+    }),
+  }),
+});
+
+export const { useContactsQuery } = contactsApi;
