3893319ec2dcb873f93114217756440718c548c8
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-04-23 18:34:22 +0300

    Refactor index.tsx to use Redux store and Provider component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,29 +1,19 @@
+import { useContactsQuery } from "../services/ContactsApi";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { toast } from "react-toastify";
 import "./Home.css";
 
-const data = [
-  {
-    name: "Adam Marcus",
-    email: "adam123@gmail.com",
-    contact: "83673677367635",
-    id: 1,
-  },
-  {
-    name: "Tom Cruise",
-    email: "tom@gmail.com",
-    contact: "83786646476",
-    id: 2,
-  },
-  {
-    name: "James Bond",
-    email: "james@gmail.com",
-    contact: "9484785787",
-    id: 3,
-  },
-];
-
 const Home = () => {
+  const { data, error, isLoading, isSuccess, isError, isFetching } =
+    useContactsQuery();
+
+  useEffect(() => {
+    if (isError) {
+      toast.error("Something went wrong");
+    }
+  });
+
   const handleDelete = async (id: any) => {
     if (window.confirm("Are you sure that you wanted to delete that user ?")) {
       toast.success("Contact Deleted Successfully");
